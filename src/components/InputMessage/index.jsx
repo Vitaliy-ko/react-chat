94e58fe898b5649613bcc4c29ee64b867ec3f7b0
Vitaliy_Kalenichenko <vitaliy.kalenichenko@gmail.com>
2020-07-19 04:10:12 +0300

    initial commit

--- src/components/InputMessage/index.jsx
+++ src/components/InputMessage/index.jsx
@@ -21,17 +21,18 @@ export const InputMessage = ({ message, isEditMessageMode, setMessage, sendMessa
   return (
     <div className={styles.container}>
       <textarea
+        tabIndex="1"
         className={styles.inputMessage}
         onChange={(e) => inputMessageHandler(e.target.value)}
         value={message}
         placeholder="Enter your message "
       ></textarea>
       {isEditMessageMode ? (
-        <button type="submit" className={styles.sendMessage} onClick={() => editMessageHandler(message)}>
+        <button type="submit" className={styles.sendMessage} onClick={() => editMessageHandler(message)} tabIndex="3">
           Edit
         </button>
       ) : (
-        <button type="submit" className={styles.sendMessage} onClick={() => sendMessageHandler(message)}>
+        <button type="submit" className={styles.sendMessage} onClick={() => sendMessageHandler(message)} tabIndex="2">
           Send
         </button>
       )}
